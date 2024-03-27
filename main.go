bfe9adcf6ed2e51030033a7922d18a8ac41ad395
Björn Albers <bjoernalbers@gmail.com>
2024-03-27 18:05:26 +0100

    Add ProfileHandler type.

--- main.go
+++ main.go
@@ -14,11 +14,16 @@ const bodyUnauthorized = `<?xml version="1.0" encoding="UTF-8"?>
 <Message>Invalid username or password</Message>
 </Error>`
 
+type ProfileHandler struct {
+}
+
+func (p ProfileHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	http.Error(w, bodyUnauthorized, http.StatusUnauthorized)
+}
+
 func main() {
 	addr := flag.String("addr", "127.0.0.1:8080", "Address to listen on")
 	flag.Parse()
-	http.HandleFunc("/rest/GetUserlogin", func(w http.ResponseWriter, r *http.Request) {
-		http.Error(w, bodyUnauthorized, http.StatusUnauthorized)
-	})
+	http.Handle("/rest/GetUserlogin", ProfileHandler{})
 	log.Fatal(http.ListenAndServe(*addr, nil))
 }
