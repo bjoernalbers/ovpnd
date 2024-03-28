76a9bad102e47524a64f7d66b4da9133517fb6aa
Björn Albers <bjoernalbers@gmail.com>
2024-03-28 13:17:28 +0100

    Move database into own file.

--- main.go
+++ main.go
@@ -3,42 +3,9 @@ package main
 
 import (
 	"flag"
-	"io"
 	"log"
 	"net/http"
-	"os"
-	"path/filepath"
-	"strings"
 )
-
-type Profile struct {
-	Path, Password string
-}
-
-type database map[string]Profile
-
-func buildDatabase(dir string) (database, error) {
-	db := database{}
-	entries, err := os.ReadDir(dir)
-	if err != nil {
-		return db, err
-	}
-	for _, entry := range entries {
-		suffix := filepath.Ext(entry.Name())
-		if suffix != ".ovpn" {
-			continue
-		}
-		name := strings.TrimSuffix(entry.Name(), suffix)
-		passwordFile, err := os.Open(filepath.Join(dir, name+".txt"))
-		if err != nil {
-			continue
-		}
-		defer passwordFile.Close()
-		password, err := io.ReadAll(passwordFile)
-		db[name] = Profile{filepath.Join(dir, entry.Name()), strings.TrimSuffix(string(password), "\n")}
-	}
-	return db, nil
-}
 
 func main() {
 	dir := flag.String("dir", "", "Directory with configuration profiles (.ovpn) and password files (.txt)")
