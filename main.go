a407a9ee31301a08b72b1a5e8575ed3b61496dc2
Björn Albers <bjoernalbers@gmail.com>
2024-03-28 10:00:16 +0100

    Add buildDatabase function.

--- main.go
+++ main.go
@@ -4,8 +4,12 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
 )
 
 const bodyUnauthorized = `<?xml version="1.0" encoding="UTF-8"?>
@@ -16,7 +20,7 @@ const bodyUnauthorized = `<?xml version="1.0" encoding="UTF-8"?>
 </Error>`
 
 type Profile struct {
-	Password, Content string
+	Path, Password string
 }
 
 type database map[string]Profile
@@ -36,13 +40,39 @@ func (db database) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, bodyUnauthorized, http.StatusUnauthorized)
 		return
 	}
-	fmt.Fprintf(w, profile.Content)
+	fmt.Fprintf(w, "content of profile\n")
+}
+
+func buildDatabase(dir string) (database, error) {
+	db := database{}
+	entries, err := os.ReadDir(dir)
+	if err != nil {
+		return db, err
+	}
+	for _, entry := range entries {
+		suffix := filepath.Ext(entry.Name())
+		if suffix != ".ovpn" {
+			continue
+		}
+		name := strings.TrimSuffix(entry.Name(), suffix)
+		passwordFile, err := os.Open(filepath.Join(dir, name+".txt"))
+		if err != nil {
+			continue
+		}
+		defer passwordFile.Close()
+		password, err := io.ReadAll(passwordFile)
+		db[name] = Profile{filepath.Join(dir, entry.Name()), strings.TrimSuffix(string(password), "\n")}
+	}
+	return db, nil
 }
 
 func main() {
 	addr := flag.String("addr", "127.0.0.1:8080", "Address to listen on")
 	flag.Parse()
-	db := database{"johndoe": Profile{"secret", "content of profile\n"}}
+	db, err := buildDatabase("testdata")
+	if err != nil {
+		log.Fatal(err)
+	}
 	http.Handle("/rest/GetUserlogin", db)
 	log.Fatal(http.ListenAndServe(*addr, nil))
 }
---
+++ main_test.go
@@ -0,0 +1,18 @@
+package main
+
+import "testing"
+
+func TestBuildDatabase(t *testing.T) {
+	db, err := buildDatabase("testdata")
+	if err != nil {
+		t.Fatalf("buildDatabase() error = %v", err)
+	}
+	want := Profile{Path: "testdata/johndoe.ovpn", Password: "secret"}
+	got, ok := db["johndoe"]
+	if !ok {
+		t.Fatalf("buildDatabase does not include expected profile: %#v", db)
+	}
+	if got != want {
+		t.Fatalf("database:\ngot:  %#v\nwant: %#v\n", got, want)
+	}
+}
